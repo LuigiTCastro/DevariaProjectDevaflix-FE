88f28725b86e4293cdeab4a011e38bc6fed05a51
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-09-19 19:54:57 -0300

    criar as validaçoes do login e cadastro

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,36 +1,95 @@
-import logo from '../assets/imagens/logo.svg'
-import iconEmail from '../assets/imagens/imgEmail.svg';
-import iconChave from '../assets/imagens/imgChave.svg';
-import { PublicInput } from '../components/General/PublicInput';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import logo from "../assets/imagens/logo.svg";
+import iconEmail from "../assets/imagens/imgEmail.svg";
+import iconChave from "../assets/imagens/imgChave.svg";
+import { PublicInput } from "../components/General/PublicInput";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { validarEmail, validarSenha } from "../utils/validators"
+import { useSearchParams } from "react-router-dom";
 
 
 export const Login = () => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-   const [login, setLogin] = useState('');
-   const [password, setPassword] = useState('');
-   return(
-    
-      <div className="ContainerPublic">
-       <img src={logo} alt='Logo Devaflix' className="logo" />
-        <form>
-           
-        <PublicInput icon={iconEmail} alt="Login" name="Login" placeholder="Login" type="text"
-          modelValue={login} setValue={setLogin} />
+  const [searchParams] = useSearchParams();
+  const success = searchParams.get("success");
 
-        <PublicInput icon={iconChave} alt="Senha" name="Senha" placeholder="Senha" type="password"
-          modelValue={password} setValue={setPassword} />
 
-            <button type='button' className='$DesktopBreakpoint'> Login
-            </button>
+  const doLogin = async () => {
+    try {
+      setError("");
 
-            <div className='link'>
-                    <p> Não possui uma conta? </p>
-                    <Link to='/register'> Faça seu cadastro agora!</Link>
-                </div>
-        </form>    
-     </div>
-  
-   )
-}
+      // Realize a validação dos campos usando as funções de validação
+      if (!validarEmail(login)) {
+        return setError("Endereço de e-mail ou senha inválido.");
+      }
+
+      if (!validarSenha(password)) {
+        return setError("Endereço de e-mail ou senha inválido.");
+      }
+
+      setLoading(true);
+      // TODO - banco de dados
+
+      setLoading(false);
+    } catch (e) {
+      console.log("Erro ao efetuar login:", e);
+      setLoading(false);
+      // TODO
+
+      return setError("Erro ao efetuar login, tente novamente");
+    }
+  };
+
+  return (
+    <div className="ContainerPublic">
+      <img src={logo} alt="Logo Devaflix" className="logo" />
+      <form>
+        {error && <p className="error">{error}</p>}
+        {success && (
+          <p className="success">
+            Cadastro efetuado com sucesso, faça seu login.
+          </p>
+        )}
+
+        <PublicInput
+          icon={iconEmail}
+          alt="Login"
+          name="Login"
+          placeholder="Login"
+          type="text"
+          modelValue={login}
+          setValue={setLogin}
+        />
+
+        <PublicInput
+          icon={iconChave}
+          alt="Senha"
+          name="Senha"
+          placeholder="Senha"
+          type="password"
+          modelValue={password}
+          setValue={setPassword}
+        />
+
+        <button
+          type="button"
+          className="$DesktopBreakpoint"
+          onClick={doLogin}
+          disabled={loading}
+        >
+          {loading ? "...Carregando" : "Login"}
+         
+        </button>
+
+        <div className="link">
+          <p> Não possui uma conta? </p>
+          <Link to="/register"> Faça seu cadastro agora!</Link>
+        </div>
+      </form>
+    </div>
+  );
+};
--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -1,44 +1,120 @@
-import logo from '../assets/imagens/logo.svg';
-import iconUser from '../assets/imagens/imgUser.svg';
-import iconEmail from '../assets/imagens/imgEmail.svg';
-import iconChave from '../assets/imagens/imgChave.svg';
-import { PublicInput } from '../components/General/PublicInput';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { AvatarInput } from '../components/General/AvatarInput';
-import { UploadImg } from '../components/General/UploadImg';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import logo from "../assets/imagens/logo.svg";
+import iconUser from "../assets/imagens/imgUser.svg";
+import iconEmail from "../assets/imagens/imgEmail.svg";
+import iconChave from "../assets/imagens/imgChave.svg";
+import { PublicInput } from "../components/General/PublicInput";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { AvatarInput } from "../components/General/AvatarInput";
+import { UploadImg } from "../components/General/UploadImg";
+import {
+  validarNome,
+  validarEmail,
+  validarSenha,
+  validarConfirmacaoSenha,
+} from "../utils/validators";
 
 
-export const Register = () => { 
 
-   const [login, setLogin] = useState('');
-   const [password, setPassword] = useState('');
-    
-   return(
-    
-      <div className="ContainerPublic register">
-       <img src={logo} alt='Logo Devaflix' className="logo" />  
-           
-        <form>
-               <UploadImg />
-               <AvatarInput/>               
-              {/*<p className='error'> Favor preencher os campos</p>*/}      
-              <PublicInput icon={iconUser} alt="Usuario" name="Usuario" placeholder="Usuario" type="text"  modelValue={login} setValue={setLogin} />      
+export const Register = () => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [usuario, setUsuario] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [error, setError] = useState("");
 
-              <PublicInput icon={iconEmail} alt="Login" name="Login" placeholder="Login" type="text" modelValue={login} setValue={setLogin} />
+  const handleCadastro = () => {
+    setError(""); // Limpar os erros anteriores
 
-              <PublicInput icon={iconChave} alt="Senha" name="Senha" placeholder="Senha" type="password" modelValue={password} setValue={setPassword} />
+    // Realizar a validação dos campos
+    if (!validarNome(usuario)) {
+      setError("Nome deve ter pelo menos 3 caracteres.");
+      return;
+    }
 
-              <PublicInput icon={iconChave} alt="Confirme a Senha" name="Confirme a Senha" placeholder="Confirmar Senha" type="password" modelValue={password} setValue={setPassword} />  
-                        
-              <button type='button' className='$DesktopBreakpoint'> Cadastrar </button>
+    if (!validarEmail(login)) {
+      setError("Endereço de e-mail inválido.");
+      return;
+    }
 
-                <div className='link'>
-                      <p> Já possui uma conta!</p>
-                      <Link to='/'> Faça seu login agora!</Link>
-                    </div>
-        </form>    
-     </div>
-  
-   )
-}
+    if (!validarSenha(password)) {
+      setError("Senha invalida!");
+      return;
+    }
+
+    if (!validarConfirmacaoSenha(password, confirm)) {
+      setError("As senhas não coincidem.");
+      return;
+    }
+
+    //TODO
+    // Implemente a lógica para enviar os dados para o servidor ou fazer o que for necessário aqui.
+  };
+
+  return (
+    <div className="ContainerPublic register">
+      <img src={logo} alt="Logo Devaflix" className="logo" />
+
+      <form>
+        <AvatarInput />
+        <UploadImg />
+
+        {error && <p className="error">{error}</p>}
+        {/*<p className='error'> Favor preencher os campos</p>*/}
+        <PublicInput
+          icon={iconUser}
+          name="Usuario"
+          alt="Usuario"
+          placeholder="Usuario"
+          type="text"
+          modelValue={usuario}
+          setValue={setUsuario}
+        />
+        <PublicInput
+          icon={iconEmail}
+          alt="Login"
+          name="Login"
+          placeholder="Login"
+          type="text"
+          modelValue={login}
+          setValue={setLogin}
+        />
+
+        <PublicInput
+          icon={iconChave}
+          alt="Senha"
+          name="Senha"
+          placeholder="Senha"
+          type="password"
+          modelValue={password}
+          setValue={setPassword}
+        />
+
+        <PublicInput
+          icon={iconChave}
+          alt="Confirme a Senha"
+          name="Confirme a Senha"
+          placeholder="Confirmar Senha"
+          type="password"
+          modelValue={confirm}
+          setValue={setConfirm}
+        />
+
+        <button
+          type="button"
+          className="$DesktopBreakpoint"
+          onClick={handleCadastro}
+        >
+          {" "}
+          Cadastrar{" "}
+        </button>
+
+        <div className="link">
+          <p> Já possui uma conta!</p>
+          <Link to="/login"> Faça seu login agora!</Link>
+        </div>
+      </form>
+    </div>
+  );
+};
