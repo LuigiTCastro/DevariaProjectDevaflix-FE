d6707139856dcd7b439382e07d754b83c93c65be
SDSirius <114844732+SDSirius@users.noreply.github.com>
2023-10-13 17:52:16 -0300

    Update SearchServices.ts

--- src/Services/SearchServices.ts
+++ src/Services/SearchServices.ts
@@ -13,4 +13,12 @@ export class SearchServices extends HttpApiServices {
     async details(query: any) {
         return await this.get(`/search/movie/?${query}`);
     }
+
+    async like(id:string){
+        return await this.put(`/search/like/${id}`,{});
+    }
+
+    async dislike(id:string) {
+        return await this.put(`/search/dislike/${id}`, {});
+    }
 }
