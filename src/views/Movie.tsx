c94927c634f737616a62a3b6f2e2c575409ba612
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-10-20 16:12:06 -0300

    nao repetir filmes

--- src/views/Movie.tsx
+++ src/views/Movie.tsx
@@ -82,3 +82,81 @@ const [movie, setMovie] = useState<MovieProps | null>(null);
   </div>
 );
 };
+/*import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "../assets/styles/movie.scss";
+import { SearchServices } from "../Services/SearchServices";
+
+const searchServices = new SearchServices();
+
+interface MovieProps {
+  title: string;
+  poster: string;
+  genre: string;
+  director: string;
+  actor: string;
+  imdbRating: string;
+  plot: string;
+}
+
+export const Movie: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const imdbID = id;
+
+  const [movie, setMovie] = useState<MovieProps | null>(null);
+  const [renderedMovieIDs, setRenderedMovieIDs] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getMovie = async (imdbID: string | undefined) => {
+      if (imdbID && !renderedMovieIDs.includes(imdbID)) {
+        const query = `imdbID=${imdbID}`;
+        const details = await searchServices.details(query);
+        setMovie(details.data);
+
+        setRenderedMovieIDs([...renderedMovieIDs, imdbID]);
+      }
+    };
+    getMovie(imdbID);
+  }, [imdbID, renderedMovieIDs]);
+
+  const handleLoginClick = () => {
+    window.history.back();
+  };
+
+  return (
+    <div className="containerMovie">
+      <div className="movie-page">
+        {movie && (
+          <>
+            <h1>{movie.title}</h1>
+            <img src={movie.poster} alt={movie.title} />
+            <p>
+              <strong>Gênero:</strong> {movie.genre}
+            </p>
+            <p>
+              <strong>Diretor:</strong> {movie.director}
+            </p>
+            <p>
+              <strong>Atores:</strong> {movie.actor}
+            </p>
+            <p>
+              <strong>Avaliação IMDb:</strong> {movie.imdbRating}
+            </p>
+            <p>
+              <strong>Enredo:</strong> {movie.plot}
+            </p>
+
+            <button
+              className="btnHeaderSair"
+              type="button"
+              onClick={handleLoginClick}
+            >
+              Sair!
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+*/
